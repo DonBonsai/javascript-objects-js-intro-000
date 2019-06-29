216cde14a7d3115b40bcad202c843b7efcb9063f
Donelle Reed <donellereed@gmail.com>
2019-06-29 17:43:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -11,3 +11,8 @@ function updatePlaylist(obj, artist, songTitle) {
   Object.assign(obj, {[artist] : songTitle});
   return playlist
 }
+
+function removeFromPlaylist(obj, artist){
+  delete obj.artist
+  return obj
+}
